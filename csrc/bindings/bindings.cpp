26f321d2be7983577d931ad7a532e6e764b53e93
Willy Chan <willychan2022@gmail.com>
2025-07-24 14:12:55 -0700

    removed old perplexity cpp bindings

--- csrc/bindings/bindings.cpp
+++ csrc/bindings/bindings.cpp
@@ -1,13 +1,11 @@
 #include <torch/library.h>
 
 #include "bindings/all_to_all_ops.h"
-#include "bindings/nvshmem.h"
 #include "core/registration.h"
 
 using namespace pplx;
 
 TORCH_LIBRARY(pplx_kernels, m) {
-  register_nvshmem_ops(m);
   register_all_to_all_ops(m);
 }
 
