29953b5c7a13a16f83ffc274458e6d614f6212fa
Willy Chan <willychan2022@gmail.com>
2025-07-28 11:01:08 -0700

    added newline

--- tests/test_all_to_all.py
+++ tests/test_all_to_all.py
@@ -383,3 +383,4 @@ def _worker_test_all_to_all_multi_node(
 @pytest.mark.parametrize("out_dtype", ["float16", "bfloat16"])
 def test_all_to_all_multi_node(in_dtype: str, out_dtype: str) -> None:
     parallel_launch_from_env(_worker_test_all_to_all_multi_node, in_dtype, out_dtype)
+    
